17dd4a61a2c9e4a293364ed7ae120a29adbfbd55
rahulppr <rahulsingh.ppr@gmail.com>
2023-12-07 23:45:47 +0530

    Aug 27 2023

---
+++ Aug27/Lab005.java
@@ -0,0 +1,16 @@
+package Aug27;
+
+
+public class Lab005 {
+    public static void main(String[] amit) {
+        // It will give only exit code 0  JVM executed succes
+    }
+
+    public void main() {
+
+    }
+
+
+    // jvm -> multiples things
+}
+
---
+++ Aug27/Lab006.java
@@ -0,0 +1,12 @@
+package Aug27;
+
+public class Lab006 {
+    // Identifier - Name of the things - class Name
+    public static void main(String[] args) {
+        String name = "Pramod"; // P, r, a , m , o , d
+        name = "Dutta";
+        name = "Amit";
+        System.out.println(name);
+}
+
+}
